72b36deef94ca531c2b7403cc0cc9601a7ac7e1b
Benjamin <benjamin.komar@kabsi.at>
2023-01-10 15:09:44 +0100

    publishing

---
+++ README.md
@@ -0,0 +1,37 @@
+# Bidirectional Enums
+A macro to automatically generate a two-way binding between an `enum`-type and any other type.
+
+## Example
+```rust
+use std::error::Error;
+
+#[macro_use]
+extern crate bidirectional_enum;
+
+bi_enum! {
+    #[derive(Debug)]
+    enum SomeEnum <=> char 
+    {
+        T1 <=> 'a'
+        T2 <=> 'b'
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let t1 = SomeEnum::T1;
+
+    // from enum to char
+    let a = char::from(t1); 
+    dbg!(&a); // 'a'
+
+    // from char back to enum
+    let t1 = SomeEnum::try_from(a);
+    dbg!(&t1); // Ok(SomeEnum::T1)
+
+    // invalid char
+    let err = SomeEnum::try_from('c');
+    dbg!(&err); // Err(EnumTryFromErr { from: "char", to: "SomeEnum" })
+
+    Ok(())
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,55 @@
-macro_rules! bi_enum {
-    ($(#[$derives:meta])* $vis:vis enum $name:ident <=> $type:tt {$($(#[$field_derives:meta])* $field_name:ident <=> $val:expr )*}) => {
-        $(#[$derives])*
-        $vis enum $name {
-            $(
-                $(#[$field_derives])* 
-                $field_name,
-            )*
-        }
+use std::{error::Error, fmt};
 
-        impl From<$name> for $type {
-            fn from(from: $name) -> Self {
-                match from {
-                    $($name::$field_name => $val,)*
-                }
-            }
-        } 
-
-        impl TryFrom<$type> for $name {
-            type Error = ();
-
-            fn try_from(try_from: char) -> Result<Self, Self::Error> {
-                match try_from {
-                    $($val => Ok($name::$field_name),)*
-                    _ => Err(())
-                }
-            }
-        } 
-    };
+#[derive(Debug)]
+pub struct EnumTryFromError {
+    from: String,
+    to: String
 }
 
-bi_enum! {
-    #[derive(Debug)]
-    enum A <=> char 
-    {
-        T1 <=> 'a'
-        T2 <=> 'b'
+impl EnumTryFromError {
+    pub fn new(from: String, to: String) -> Self {
+        EnumTryFromError { from, to }
+    }
+}
+
+impl fmt::Display for EnumTryFromError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Failed to convert `{}` into `{}`", self.from, self.to)
+    }
+}
+
+impl Error for EnumTryFromError {}
+
+pub mod bi_enum {
+    #[macro_export]
+    macro_rules! bi_enum {    
+        ($(#[$derives:meta])* $vis:vis enum $name:ident <=> $type:tt {$($(#[$field_derives:meta])* $field_name:ident <=> $val:expr )*}) => {
+            $(#[$derives])*
+            $vis enum $name {
+                $(
+                    $(#[$field_derives])* 
+                    $field_name,
+                )*
+            }
+    
+            impl From<$name> for $type {
+                fn from(from: $name) -> Self {
+                    match from {
+                        $($name::$field_name => $val,)*
+                    }
+                }
+            } 
+    
+            impl TryFrom<$type> for $name {
+                type Error = $crate::EnumTryFromError;
+    
+                fn try_from(try_from: char) -> Result<Self, Self::Error> {
+                    match try_from {
+                        $($val => Ok($name::$field_name),)*
+                        _ => Err($crate::EnumTryFromError::new(stringify!($type).to_string(), stringify!($name).to_string()))
+                    }
+                }
+            } 
+        };
     }
 }
